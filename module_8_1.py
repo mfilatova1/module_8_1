4be91a58d0c96e1e09ea8ee90201a9c3db01522e
mfilatova1 <filatovam435@gmail.com>
2024-08-24 10:24:36 +0300

    Add files via upload

---
+++ module_8_1.py
@@ -0,0 +1,11 @@
+def add_everything_up(a, b):
+    try:
+        summa = a + b
+    except TypeError:
+        return f'{a}{b}'
+    else:
+        return round(summa, 3)
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
